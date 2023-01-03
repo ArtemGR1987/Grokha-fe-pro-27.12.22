5823067aa8854610ae8641fb3d85c535c53d5d3a
Artem Grokha <grokha.artem@gmail.com>
2023-01-03 16:17:32 +0200

    Update index.js

---
+++ index.js
@@ -0,0 +1,5 @@
+let userName = prompt("What is your name?");
+let userSurname = prompt("What is your surname?");
+let userAge = prompt("How old are you?");
+
+console.log(`${userName} ${userSurname} ${userAge} years old`);
