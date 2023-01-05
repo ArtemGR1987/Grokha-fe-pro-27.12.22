6cd26a554c39caf76ca4d1535a7c15af94e4920f
Artem Grokha <grokha.artem@gmail.com>
2023-01-05 14:07:42 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,12 +3,12 @@ let userName = prompt("Назвіть ваше ім'я, будь ласка");
 let userAge = +prompt("Скільки вам років?");
 let userSmoke = confirm("Ви палите?");
 
-if ((userAge >= 18, userSmoke)) {
+if (userAge >= 18 && userSmoke) {
   alert(`Привіт ${userName}, що ж, ти сам обрав цей шлях...`);
-} else if ((userAge >= 18, userSmoke === false)) {
+} else if (userAge >= 18 && userSmoke === false) {
   alert(`Привіт ${userName}, чудово! Ти мабуть ще й спортом займаєшся!`);
-} else if ((userAge < 18, userSmoke)) {
+} else if (userAge < 18 && userSmoke) {
   alert(`Привіт ${userName}! A батьки знають про це?`);
-} else if ((userAge < 18, userSmoke === false)) {
+} else if (userAge < 18 && userSmoke === false) {
   alert(`Привіт ${userName}! Правильно, зараз головне добре вчитися!`);
 }
