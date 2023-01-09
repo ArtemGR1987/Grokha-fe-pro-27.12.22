03f06ff2ecd7df12f93387c8f64ba01a6fc1a454
Artem Grokha <grokha.artem@gmail.com>
2023-01-09 15:15:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 "use strict";
+let answerUser = prompt("Що ви хочете зробити(add, sub, mult,div)?");
+let firstNumber = +prompt("Введіть перше число");
+let secondNumber = +prompt("Введіть друге число");
+let add = firstNumber + secondNumber;
+let sub = firstNumber - secondNumber;
+let mult = firstNumber * secondNumber;
+let div = firstNumber / secondNumber;
+if (answerUser === "add") {
+  alert(`${firstNumber} + ${secondNumber} = ${add}`);
+} else if (answerUser === "sub") {
+  alert(`${firstNumber} - ${secondNumber} = ${sub}`);
+} else if (answerUser === "mult") {
+  alert(`${firstNumber} * ${secondNumber} = ${mult}`);
+} else if (answerUser === "div") {
+  alert(`${firstNumber} / ${secondNumber} = ${div}`);
+}
