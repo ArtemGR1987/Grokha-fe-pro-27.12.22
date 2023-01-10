298ccbd360fb5f57306bc24172adf122e5e76b77
Artem Grokha <grokha.artem@gmail.com>
2023-01-10 12:10:12 +0200

    Исправления после проверки

--- index.js
+++ index.js
@@ -2,16 +2,16 @@
 let answerUser = prompt("Що ви хочете зробити(add, sub, mult,div)?");
 let firstNumber = +prompt("Введіть перше число");
 let secondNumber = +prompt("Введіть друге число");
-let add = firstNumber + secondNumber;
-let sub = firstNumber - secondNumber;
-let mult = firstNumber * secondNumber;
-let div = firstNumber / secondNumber;
+//let add = firstNumber + secondNumber;
+//let sub = firstNumber - secondNumber;
+//let mult = firstNumber * secondNumber;
+//let div = firstNumber / secondNumber;
 if (answerUser === "add") {
-  alert(`${firstNumber} + ${secondNumber} = ${add}`);
+  alert(`${firstNumber} + ${secondNumber} = ${firstNumber + secondNumber}`);
 } else if (answerUser === "sub") {
-  alert(`${firstNumber} - ${secondNumber} = ${sub}`);
+  alert(`${firstNumber} - ${secondNumber} = ${firstNumber - secondNumber}`);
 } else if (answerUser === "mult") {
-  alert(`${firstNumber} * ${secondNumber} = ${mult}`);
+  alert(`${firstNumber} * ${secondNumber} = ${firstNumber * secondNumber}`);
 } else if (answerUser === "div") {
-  alert(`${firstNumber} / ${secondNumber} = ${div}`);
+  alert(`${firstNumber} / ${secondNumber} = ${firstNumber / secondNumber}`);
 }
