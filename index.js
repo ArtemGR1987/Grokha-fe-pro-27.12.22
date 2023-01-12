74a42ad69f667550f6e185560689f08c280bfa1b
Artem Grokha <grokha.artem@gmail.com>
2023-01-12 21:02:17 +0200

    Update index.js

--- index.js
+++ index.js
@@ -8,12 +8,11 @@ const userUkraine = "Украины";
 const userUSA = "США";
 const userEngland = "Англии";
 
-//Виды спорта и чемпионы
-const footballPlayer = "Криштиану Роналду";
-const basketballPlayer = "Майкл Джордан";
-const hockeyPlayer = "Уэйн Гретцки";
+if (userAge === "Null") {
+  alert("Жаль, что Вы не захотели ввести свой год рождения");
+}
 
-if (userCity === "Киев" && userSport === "Футбол") {
+if (userCity === "Киев") {
   alert(`${userAge} / Ты живёшь в столице ${userUkraine}`);
 } else if (userCity === "Вашингтон") {
   alert(`${userAge} / Ты живёшь в столице ${userUSA}`);
@@ -21,4 +20,17 @@ if (userCity === "Киев" && userSport === "Футбол") {
   alert(`${userAge} / Ты живёшь в столице ${userEngland}`);
 } else {
   alert(`${userAge} / Ты живёшь в городе ${userCity}`);
+}
+
+//Виды спорта и чемпионы
+const footballPlayer = "Криштиану Роналду";
+const basketballPlayer = "Майкл Джордан";
+const hockeyPlayer = "Уэйн Гретцки";
+
+if (userSport === "Футбол") {
+  alert(`Круто! Хочешь стать как ${footballPlayer}?`);
+} else if (userSport === "Баскетбол") {
+  alert(`Круто! Хочешь стать как ${basketballPlayer}?`);
+} else if (userSport === "Хоккей") {
+  alert(`Круто! Хочешь стать как ${hockeyPlayer}?`);
 }
