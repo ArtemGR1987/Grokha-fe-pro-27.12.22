1fade53cdd2cd8514920da1b0bd3a3758c51f43a
Artem Grokha <grokha.artem@gmail.com>
2023-01-12 14:00:24 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 "use strict";
+
+let userAge = +prompt("В каком году вы родились?");
+let userCity = prompt("В каком городе вы проживаете?");
+let userSport = prompt("Какой ваш любимый вид спорта?");
