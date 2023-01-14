b004296cfb3b46b466563c1000ffef84f636a0b8
Artem Grokha <grokha.artem@gmail.com>
2023-01-14 13:30:16 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,21 +1,40 @@
 "use strict";
 
 let userAge = prompt("В каком году вы родились?", "");
-let userCity = prompt("В каком городе вы проживаете?");
+if (userAge === null) {
+  alert("Жаль, что Вы не захотели ввести свой год рождения");
+}
+let userCity = prompt("В каком городе вы проживаете?", "");
+if (userCity === null) {
+  alert("Жаль, что Вы не захотели ввести свой город");
+}
 let userSport = prompt("Какой ваш любимый вид спорта?");
+if (userSport === null) {
+  alert("Жаль, что Вы не захотели ввести свой любимый вид спорта");
+}
 //Страны
 const userUkraine = "Украины";
 const userUSA = "США";
 const userEngland = "Англии";
 
+if (userAge === null && userCity === null && userSport === null) {
+  alert("Жаль, что Вы не захотели ответить ни на один вопрос");
+}
+
 if (userCity === "Киев") {
-  alert(`${userAge} / Ты живёшь в столице ${userUkraine}`);
+  alert(`Ты родился в ${userAge} году / Ты живёшь в столице ${userUkraine}`);
 } else if (userCity === "Вашингтон") {
-  alert(`${userAge} / Ты живёшь в столице ${userUSA}`);
+  alert(`Ты родился в ${userAge} году / Ты живёшь в столице ${userUSA}`);
 } else if (userCity === "Лондон") {
-  alert(`${userAge} / Ты живёшь в столице ${userEngland}`);
+  alert(`Ты родился в ${userAge} году / Ты живёшь в столице ${userEngland}`);
+} else if (userCity === null && userAge === null) {
+  alert(`Год рождения не указан / Город не указан`);
+} else if (userCity === null) {
+  alert(`${userAge} / Город не указан`);
+} else if (userAge === null) {
+  alert(`Год рождения не указан / Ты живёшь в городе ${userCity}`);
 } else if (userAge) {
-  alert(`${userAge} / Ты живёшь в городе ${userCity}`);
+  alert(`Ты родился в ${userAge} году / Ты живёшь в городе ${userCity}`);
 }
 
 //Виды спорта и чемпионы
@@ -24,20 +43,22 @@ const basketballPlayer = "Майкл Джордан";
 const hockeyPlayer = "Уэйн Гретцки";
 
 if (userSport === "Футбол") {
-  alert(`Круто! Хочешь стать как ${footballPlayer}?`);
+  alert(`Любишь футбол! Круто! Хочешь стать как ${footballPlayer}?`);
 } else if (userSport === "Баскетбол") {
-  alert(`Круто! Хочешь стать как ${basketballPlayer}?`);
+  alert(`Любишь баскетбол! Круто! Хочешь стать как ${basketballPlayer}?`);
 } else if (userSport === "Хоккей") {
-  alert(`Круто! Хочешь стать как ${hockeyPlayer}?`);
+  alert(`Любишь хоккей! Круто! Хочешь стать как ${hockeyPlayer}?`);
 }
 
 //Если пользователь не захотел вводить данные
-if (userAge === null && userCity === null && userSport === null) {
-  alert("Жаль, что Вы не захотели ответить ни на один вопрос");
-} else if (userAge === null) {
-  alert("Жаль, что Вы не захотели ввести свой год рождения");
-} else if (userCity === null) {
-  alert("Жаль, что Вы не захотели ввести свой город");
-} else if (userSport === null) {
-  alert("Жаль, что Вы не захотели ввести свой любимый вид спорта");
-}
+//if (userAge === null && userCity === null && userSport === null) {
+//alert("Жаль, что Вы не захотели ответить ни на один вопрос");
+//}
+//if (userAge === null) {
+//alert("Жаль, что Вы не захотели ввести свой год рождения");
+//} //else if (userCity === null) {
+//alert("Жаль, что Вы не захотели ввести свой город");
+//}
+//else if (userSport === null) {
+//alert("Жаль, что Вы не захотели ввести свой любимый вид спорта");
+//}
