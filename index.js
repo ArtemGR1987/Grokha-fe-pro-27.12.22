47c30bfde1cb6832627a91fee056c2bd9eac49ff
Artem Grokha <grokha.artem@gmail.com>
2023-01-30 12:57:00 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 "use strict";
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+
+function removeElement(array, item) {
+  return array.filter(function (element) {
+    return element != item;
+  });
+}
+console.log(array);
+console.log(removeElement(array, 3));
