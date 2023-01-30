37b4f8b4e79e6fe21407020644ff6e613a774132
Artem Grokha <grokha.artem@gmail.com>
2023-01-30 14:47:36 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 "use strict";
+const characters =
+  "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+function generateKey(length) {
+  let result = " ";
+  const charactersLength = characters.length;
+  //console.log(charactersLength);
+  for (let i = 0; i < length; i++) {
+    result += characters.charAt(Math.floor(Math.random() * charactersLength));
+  }
+
+  return result;
+}
+
+console.log(generateKey(11));
