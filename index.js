39126a0f5f25e9fa6662ffe4b0ca80d058361f1a
Artem Grokha <grokha.artem@gmail.com>
2023-02-06 14:44:55 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+function getAdd() {
+  let result = 0;
+  return function (number) {
+    return (result += number);
+  };
+}
+
+const sum = getAdd();
+
+console.log(sum(3));
+console.log(sum(5));
+console.log(sum(7));
+console.log(sum(9));
