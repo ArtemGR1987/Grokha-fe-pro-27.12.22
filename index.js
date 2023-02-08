d24844b161d83ed4da9834136acc9e807b45667a
Artem Grokha <grokha.artem@gmail.com>
2023-02-08 17:44:22 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 "use strict";
+function pow(num, degree) {
+  if (degree == 1) {
+    return num;
+  } else {
+    return num * pow(num, degree - 1);
+  }
+}
+
+console.log(pow(5, 3));
+console.log(pow(10, 4));
