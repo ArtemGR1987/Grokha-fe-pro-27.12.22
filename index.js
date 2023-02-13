fea5c7b4622431e59aca8d0a7b50c7c16004ad7e
Artem Grokha <grokha.artem@gmail.com>
2023-02-13 14:35:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 "use strict";
 // Дано масив з елементами різних типів.
 //Створити функцію яка вираховує середнє арифметичне лише числових елементів даного масиву.
-// const arr = [1, "Hello", null, { name: "John", age: 35 }, 77, 18];
-// console.log(arr);
+
+function averageSum(arr) {
+  let newArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "number") {
+      newArr.push(arr[i]);
+      //   console.log(arr[i]);
+    }
+  }
+  //   console.log(newArr.length);
+  //   console.log(newArr);
+
+  return newArr.reduce((acc, num) => (acc += num), 0) / newArr.length;
+}
+
+console.log(averageSum([30, "Hello", null, 20, 30, 40]));
