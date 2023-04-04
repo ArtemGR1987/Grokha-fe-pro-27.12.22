382d20c414c17ae3aba4e25c309bb5d45f8af090
Artem Grokha <grokha.artem@gmail.com>
2023-04-04 15:14:25 +0300

    Изменение кнопки Комментарии

--- index.js
+++ index.js
@@ -1,53 +1,88 @@
 "use strict";
-const form = document.querySelector("#form");
-const postContainer = document.querySelector("#post-container");
-const commentsBtn = document.querySelector("#comments-btn");
-const commentsContainer = document.querySelector("#comments-container");
+const form = document.querySelector(".form");
+const box = document.querySelector(".post__box");
+const commentWrapp = document.querySelector(".comment__wrapp");
+const btnComment = document.querySelector(".btn__comment");
+const input = document.querySelector(".id__input");
+const requesrURL = "https://jsonplaceholder.typicode.com/";
 
-form.addEventListener("submit", (event) => {
-  event.preventDefault();
-  const postId = event.target.elements.postId.value;
-  if (postId) {
-    fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
-      .then((response) => response.json())
-      .then((post) => {
-        postContainer.innerHTML = `
-          <h2>${post.title}</h2>
-          <p>${post.body}</p>
+const createPostContainer = (title, body) => {
+  const postContainer = document.createElement("div");
+  postContainer.innerHTML = `
+            <h2> Title is - ${title}</h2>
+            <p> Body is - ${body}</p>
         `;
-        commentsBtn.style.display = "block";
+  //   btnComment.classList.add("btn__comment-active");
+
+  return postContainer;
+};
+
+const createPost = () => {
+  form.addEventListener("submit", (e) => {
+    e.preventDefault();
+    box.innerHTML = "";
+    commentWrapp.innerHTML = "";
+    if (!input.value) return;
+    fetch(`${requesrURL}posts/${input.value}`)
+      .then((res) => {
+        if (res.status <= 400) {
+          return res.json();
+        }
+        return res.json().then((err) => {
+          const e = new Error("Something wrong");
+          e.data = err;
+          const error = document.createElement("div");
+          error.innerHTML = `
+            <h2> Ошибка - введенный номер должен быть от 1 до 100</h2>
+            `;
+          btnComment.classList.remove("btn__comment-active");
+          box.appendChild(error);
+          throw e;
+        });
       })
-      .catch((error) => {
-        console.error(error);
-        alert(
-          "Не удалось загрузить пост. Проверьте правильность ID и соединение с интернетом."
-        );
+      .then((post) => {
+        const postDiv = createPostContainer(post.title, post.body);
+        box.appendChild(postDiv);
+      })
+      .catch((err) => {
+        console.error(err);
       });
-  }
-});
+  });
+};
 
-commentsBtn.addEventListener("click", () => {
-  const postId = document
-    .querySelector("#post-container h2")
-    .textContent.split(".")[0];
-  fetch(`https://jsonplaceholder.typicode.com/posts/${postId}/comments`)
-    .then((response) => response.json())
-    .then((comments) => {
-      commentsContainer.innerHTML = "";
-      comments.forEach((comment) => {
-        const li = document.createElement("li");
-        li.innerHTML = `
-          <h3>${comment.name}</h3>
-          <p>${comment.body}</p>
-          <cite>${comment.email}</cite>
-        `;
-        commentsContainer.appendChild(li);
+createPost();
+
+const createCommentsContainer = (name, body) => {
+  const commentBox = document.createElement("div");
+  commentBox.innerHTML = `
+        <h2> Comment: ${name}</h2>
+        <p> Comment body: ${body}</p>
+    `;
+
+  return commentBox;
+};
+
+const createComments = () => {
+  btnComment.addEventListener("click", () => {
+    commentWrapp.innerHTML = "";
+    fetch(`${requesrURL}posts/${input.value}/comments`)
+      .then((res) => {
+        return res.json();
+      })
+      .then((comments) => {
+        comments.forEach((comment) => {
+          const commentBox = createCommentsContainer(
+            comment.name,
+            comment.body
+          );
+          commentWrapp.appendChild(commentBox);
+        });
+        btnComment.classList.add("btn__comment-active"); // добавить эту строку
+      })
+      .catch((err) => {
+        console.error(err);
       });
-    })
-    .catch((error) => {
-      console.error(error);
-      alert(
-        "Не удалось загрузить комментарии. Проверьте соединение с интернетом."
-      );
-    });
-});
+  });
+};
+
+createComments();
