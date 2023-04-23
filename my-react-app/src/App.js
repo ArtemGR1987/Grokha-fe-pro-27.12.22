ca0bb461e56047c7bf92fde5b77752b2deb2d95b
Artem Grokha <grokha.artem@gmail.com>
2023-04-23 21:32:20 +0300

    Подключил реакт и внёс изменения

---
+++ my-react-app/src/App.js
@@ -0,0 +1,32 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <h1>Мій перший реакт додаток</h1>
+      </header>
+      <nav>
+        <ul>
+          <li>
+            <a href="https://www.w3schools.com/react/react_getstarted.asp">
+              Пункт меню 1
+            </a>
+          </li>
+          <li>
+            <a href="https://react.dev/learn">Пункт меню 2</a>
+          </li>
+          <li>
+            <a href="https://create-react-app.dev/docs/getting-started/">
+              Пункт меню 3
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <main>
+        <h2>Ласкаво просимо до мого додатку!</h2>
+        <p>Це мій перший React додаток з трьома блоками.</p>
+      </main>
+    </div>
+  );
+}
+
+export default App;
